ef2365a8b586ce6f1e38669b3409c7e56f802f28
giua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <gianghainam.edu@2021@outlook.com.vn>
2026-04-19 18:00:44 +0700

    another rebased

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -155,19 +155,18 @@ export default function SearchModal({
 return (
   <Transition appear show={searchOpen} as={Fragment}>
     <Dialog as="div" className="fixed inset-0 z-[200]" onClose={closeSearchBox}>
-      <div className="fixed inset-0" onClick={closeSearchBox} />
-      <div className="fixed inset-0 flex items-start justify-center px-4 pt-12">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-100"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-100"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          {/* <div className="w-full max-w-3xl transform overflow-hidden rounded border border-gray-300/30 text-left shadow-xl transition-all backdrop-blur-lg"> */}
-          <div className="fixed left-1/2 top-12 -translate-x-1/2 w-full max-w-3xl transform overflow-hidden rounded border border-gray-300/30 text-left shadow-xl transition-all backdrop-blur-lg">
+  <div className="fixed inset-0" onClick={closeSearchBox} />
+  <div className="fixed inset-0 flex items-start justify-center px-4 pt-12 pointer-events-none">
+    <Transition.Child
+      as={Fragment}
+      enter="ease-out duration-100"
+      enterFrom="opacity-0 scale-95"
+      enterTo="opacity-100 scale-100"
+      leave="ease-in duration-100"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <div className="pointer-events-auto w-full max-w-3xl overflow-hidden rounded border border-gray-300/30 text-left shadow-xl backdrop-blur-lg">
               <Dialog.Title
                 as="h3"
                 className="flex items-center space-x-4 border-b border-gray-400/30 bg-black/60 p-4 text-white"
