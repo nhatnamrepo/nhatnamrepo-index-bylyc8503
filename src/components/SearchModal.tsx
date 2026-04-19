d9c14faed58263fc78a0dec1eba9ecdef2531aeb
giua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <gianghainam.edu@2021@outlook.com.vn>
2026-04-19 17:33:26 +0700

    final?

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -156,7 +156,7 @@ export default function SearchModal({
     <Transition appear show={searchOpen} as={Fragment}>
       <Dialog as="div" className="fixed inset-0 z-[200] overflow-y-auto" onClose={closeSearchBox}>
         <div className="fixed inset-0" onClick={closeSearchBox} />
-        <div className="min-h-screen px-4 flex items-start justify-center">
+        <div className="min-h-screen flex items-start justify-center px-4">
 
           <Transition.Child
             as={Fragment}
@@ -167,7 +167,9 @@ export default function SearchModal({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="my-12  w-full max-w-3xl transform overflow-hidden rounded border border-gray-300/30 text-left shadow-xl transition-all backdrop-blur-lg">
+            {/*mx auto is good? */}
+            
+           <div className="my-12 mx-auto w-full max-w-3xl transform overflow-hidden rounded border border-gray-300/30 text-left shadow-xl transition-all backdrop-blur-lg">
               <Dialog.Title
                 as="h3"
                 className="flex items-center space-x-4 border-b border-gray-400/30 bg-black/60 p-4 text-white"
