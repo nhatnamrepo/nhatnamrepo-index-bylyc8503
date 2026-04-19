fe44c2ab16208ca6e6e95646b18382f6965f8c57
giua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <gianghainam.edu@2021@outlook.com.vn>
2026-04-19 17:07:47 +0700

    Local better than another CDN ig

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -21,7 +21,7 @@ export default function Folders() {
       {/* Background Image */}
       <div 
         className="fixed inset-0 -z-10 h-full w-full bg-cover bg-center"
-        style={{ backgroundImage: 'url(https://raw.githubusercontent.com/nhatnamrepo/nhatnamrepo-index-bylyc8503/refs/heads/main/public/images/bg.png)' }}
+        style={{ backgroundImage: 'url(/images/bg.png)' }}
       />
 
       <main className="flex w-full flex-1 flex-col bg-black/40">
