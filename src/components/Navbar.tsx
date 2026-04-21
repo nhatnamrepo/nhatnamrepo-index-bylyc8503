ad87dcaf004647abe9583c51abd86f7630b2ea2d
tinhdaysauconmeanhchinhohaitayhaicumombucuemtrongconmeroianhnhinlenbautroigiua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <97789236+chemchetchagio@users.noreply.github.com>
2026-04-21 22:30:06 +0700

    MORE, MORE

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white bg-opacity-80 backdrop-blur-md dark:border-gray-500/30 dark:bg-gray-900">
+    <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white/80 backdrop-blur-lg dark:border-gray-600/30 dark:bg-gray-900/70">
       <Toaster />
 
       <SearchModal searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
